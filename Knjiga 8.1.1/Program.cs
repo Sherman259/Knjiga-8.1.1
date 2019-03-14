24f9f4f7d687b9d8420966b80b93f73a33a75207
Tihomir Jovic <jtihomir@gmail.com>
2019-03-14 21:38:49 +0100

    navodnici

--- Knjiga 8.1.1/Program.cs
+++ Knjiga 8.1.1/Program.cs
@@ -10,6 +10,51 @@ namespace Knjiga_8._1._1
     {
         static void Main(string[] args)
         {
+            Console.Write("Unesite naziv proizvoda");
+            Proizvod proizvod = new Proizvod(Console.ReadLine);
+
+            Console.Write("Unesite cijenu proizvoda:");
+            prozvod.Cijena = double.Parse(Console.ReadLine());
+            Console.Write("Unesite marzu proizvoda:");
+            proizvod.marza = double.Parse(Console.ReadLine());
+            Console.Write("Unesite porez na proizvoda:");
+            proizvod.Porez = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Maloprodajna cijena za proizvoda {0} je {1}.", proizvod.Naziv,proizvod.MPC());
+
+           Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
         }
     }
+
+    internal class Proizvod
+    {
+        //Polja
+        public string Naziv = "ime";
+        public double Cijena = 0;
+        public double Marza = 0;
+        public double Porez = 0;
+
+        // Metode
+        public Proizvod(string naziv);
+     {
+        Naziv = naziv;
+     }
+    public double MPC()
+    {
+        return Cijena + Marza + Porez;
+    }
+
+       
+    
 }
